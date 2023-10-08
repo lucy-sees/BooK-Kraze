dcf368bd477529546767a39b3b8b200f9a357b6b
Lucy W. Mwangi <100368317+lucy-sees@users.noreply.github.com>
2023-10-08 17:17:39 +0300

    Update Spinner and BackButton in Showbook.jsx

--- frontend/src/pages/ShowBook.jsx
+++ frontend/src/pages/ShowBook.jsx
@@ -3,8 +3,8 @@ import React from 'react';
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useParams } from 'react-router-dom';
-import { BackButton } from '../components/backButton';
-import { Spinner } from '../components/Spinner'
+import BackButton  from '../components/backButton';
+import Spinner from '../components/Spinner';
 
 const ShowBook = () => {
     const [book, setbook] = useState([]);
